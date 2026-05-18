53880b0b2e20b372f44b1882742d83c906f61fa6
Mariano Belgrano <mbelgrano@gmail.com>
2026-05-18 02:09:21 -0300

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
-// @ts-expect-error CSS module declaration is provided by Next.js build setup
-import '@/app/ui/global.css'
-import { inter, lusitana } from '@/app/ui/fonts'
+import '@/app/ui/global.css';
+import { inter, lusitana } from '@/app/ui/fonts';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className = { `${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
